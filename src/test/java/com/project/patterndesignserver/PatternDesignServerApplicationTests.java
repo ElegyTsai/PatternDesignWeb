cfa138f3d3e0f80e423dcdce250926e47de7eb69
elegy <547442993@qq.com>
2021-05-24 16:24:00 +0900

    initiated

---
+++ src/test/java/com/project/patterndesignserver/PatternDesignServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.patterndesignserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternDesignServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
