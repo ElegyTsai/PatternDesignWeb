1cb1f8a6c225bb248c3fec8c341cf56bac816cab
elegy <547442993@qq.com>
2021-05-24 17:13:00 +0900

    add basic model of user

---
+++ src/main/java/com/project/patterndesignserver/model/member/UserRole.java
@@ -0,0 +1,10 @@
+package com.project.patterndesignserver.model.member;
+
+import lombok.Data;
+
+@Data
+public class UserRole {
+    //角色表的主键
+    private String id;
+    private String roleName;
+}
